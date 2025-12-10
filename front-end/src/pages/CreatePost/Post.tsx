2c22851a8bcd1f14299b0923b3df921749bf0123
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-10 11:49:17 +0100

    formulaire bineôt terminé en front-end

--- front-end/src/pages/CreatePost/Post.tsx
+++ front-end/src/pages/CreatePost/Post.tsx
@@ -7,15 +7,33 @@ export default function CreatePost() {
 
 
     return (
-        <div className='post-container'>
-            <div className="post">
-              <h1>New Post</h1>               
-              <button>Poster !</button>
-              </div>
-             {/* <NavBarComponent/> MS NavBarComponent à activer par la personne concernée après validation de mon travail sur NavBar mettre gohome et comment/chat */}
+        <div className="post">
+            <h1>New Post</h1>
+            <form>
+                <div>
+                    <label>
+                        Tapez votre post ici
+                        <input type="text" name="post" />
+                    </label>
+                </div>
+                <div>
+                    <label className='date'>
+                    15:25 - 13 aout 25
+                    </label>
+                </div>
+                <div className='poster'>
+                    <input type="submit" value="Poster !" />
+                </div>
+            </form>
+            {/* <NavBarComponent/> MS NavBarComponent à activer par la personne concernée après validation de mon travail sur NavBar mettre gohome et comment/chat */}
 
         </div>
-     
+
+
     )
 }
+
+
+
+//MS form exemple pris dans site https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events/ pour voir 
 
