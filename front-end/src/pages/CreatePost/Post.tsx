48c0cc924bfaf54c14f16e10f36b5737549f0b09
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-10 14:16:53 +0100

    voir Massi pour la date et title pour créaton de post

--- front-end/src/pages/CreatePost/Post.tsx
+++ front-end/src/pages/CreatePost/Post.tsx
@@ -8,22 +8,23 @@ export default function CreatePost() {
 
     return (
         <div className="post">
-            <h1>New Post</h1>
+            <h1 className="post-title">New Post</h1>
             <form className='createpost-form'>
-                <div>
-                    <label>
-                        <input type="text" name="post" value=" Tapez votre post ici" />
-                    </label>
+                <div className="textarea-container">
+                <textarea
+                    name="post"
+                    placeholder="Tapez votre post ici..."
+                    className="post-input"
+                />
+                
+                <p className="date">
+                    15:25 - 13 août 25
+                    {/* Ms afficher la date ici */}
+                </p>
                 </div>
-                <div>
-                    <label className='date'>
-                        15:25 - 13 aout 25
-                        {/* Ms afficher la date ici */}
-                    </label>
-                </div>
-                <div className='poster'>
-                    <input type="submit" value="Poster !" />
-                </div>
+                    {/* <input type="submit" value="Poster !" /> */}
+                    <button className='poster' type="submit">Poster !</button>
+                
             </form>
             {/* <NavBarComponent/> MS NavBarComponent à activer par la personne concernée après validation de mon travail sur NavBar mettre gohome et comment/chat */}
 
@@ -37,3 +38,28 @@ export default function CreatePost() {
 
 //MS form exemple pris dans site https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events/ pour voir 
 
+// import './post.css'
+
+// export default function CreatePost() {
+//     return (
+//         <div className="post">
+//             <h1>New Post</h1>
+
+//             <form className="createpost-form">
+
+//                 {/* Zone de texte */}
+
+
+//                 {/* Date */}
+//                 <p className="date">
+//                     15:25 - 13 août 25
+//                 </p>
+
+//                 {/* Bouton */}
+//                 <button type="submit" className="post-button">
+//                     Poster !
+//                 </button>
+//             </form>
+//         </div>
+//     )
+// }
