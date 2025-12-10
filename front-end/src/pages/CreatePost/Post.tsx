0a7849b458eb01b9f317e824cd3dcb288b89d414
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-10 10:02:18 +0100

    ajout stylecss code de bérangère pour le h1

--- front-end/src/pages/CreatePost/Post.tsx
+++ front-end/src/pages/CreatePost/Post.tsx
@@ -8,7 +8,7 @@ export default function CreatePost() {
 
     return (
         <div className='post'>
-              au secours je suis dans CreatePost
+              <h1>au secours je suis dans CreatePost</h1>
              {/* <NavBarComponent/> MS NavBarComponent à activer par la personne concernée après validation de mon travail sur NavBar mettre gohome et comment/chat */}
         </div>
      
