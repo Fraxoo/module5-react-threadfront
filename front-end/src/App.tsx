18d7321bc9a19cd063b40caa8cbd49669c29c64c
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-10 10:11:57 +0100

    style css modifié

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/home/Home";
 import Post from "./pages/CreatePost/Post";
-// MS ajout routes CreatePost
+import '../src/style.css';
+// MS ajout routes CreatePost et style.css le css principal
 
 
 
--- front-end/src/pages/CreatePost/Post.tsx
+++ front-end/src/pages/CreatePost/Post.tsx
@@ -8,7 +8,7 @@ export default function CreatePost() {
 
     return (
         <div className='post'>
-              <h1>au secours je suis dans CreatePost</h1>
+              <h1>New Post</h1>
              {/* <NavBarComponent/> MS NavBarComponent à activer par la personne concernée après validation de mon travail sur NavBar mettre gohome et comment/chat */}
         </div>
      
