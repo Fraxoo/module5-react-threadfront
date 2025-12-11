908e85561d70567131ee67b836257d2f16411e4b
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-11 11:38:57 +0100

    je suis en train de faire la date en react typescript

--- front-end/src/pages/CreatePost/Post.tsx
+++ front-end/src/pages/CreatePost/Post.tsx
@@ -1,51 +1,22 @@
 import './post.css'
+import { utcToZonedTime } from  'date-fns-tz' 
 // MS import NavBarComponent from "../../components/navbar/NavBarComponent" à activer par la personne concernée après validation de mon travail sur NavBar
-
-
-// export default function CreatePost() {
-
-
-
-//     return (
-//         <div className="post">
-
-//              <h1 className="post-title">New Post</h1>
-  
-//             <form className='createpost-form'>
-                            
-//                 <div className="textarea-container">
-//                 <textarea
-//                     name="post"
-//                     placeholder="Tapez votre post ici..."
-//                     className="post-input"
-//                 />
-                
-//                 <p className="date">
-//                     15:25 - 13 août 25
-//                     {/* Ms afficher la date ici */}
-//                 </p>
-//                 </div>
-//                     {/* <input type="submit" value="Poster !" /> */}
-//                     <button className='poster' type="submit">Poster !</button>
-                
-//             </form>
-//             {/* <NavBarComponent/> MS NavBarComponent à activer par la personne concernée après validation de mon travail sur NavBar mettre gohome et comment/chat */}
-
-//         </div>
-
-
-//     )
-// }
-
-
 
 //MS form exemple pris dans site https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events/ pour voir 
 
 // ancien code ci-dessus
 
-import './post.css'
-
 export default function CreatePost() {
+    const convertToTimezone = ( utcDate : string , timeZone : string ): Date => { 
+  return  utcToZonedTime (utcDate, timeZone); 
+}; 
+
+// Exemple d'utilisation 
+const tokyoTime = convertToTimezone ( "2024-11-17T10:15:30.000Z" , "Asia/Tokyo" ); 
+console.log ( tokyoTime); // Sortie
+ : '2024-11-17T19:15:30.000+09:00' 
+// Ceci est particulièrement utile pour les applications prenant en charge plusieurs régions.
+
     return (
         <div className="post">
             <div className="post-content">
@@ -67,6 +38,8 @@ export default function CreatePost() {
                     <button className='poster' type="submit">Poster !</button>
                 </form>
             </div>
+                {/* <NavBarComponent/> MS NavBarComponent à activer par la personne concernée après validation de mon travail sur NavBar mettre gohome et comment/chat */}
+
         </div>
     )
 }
