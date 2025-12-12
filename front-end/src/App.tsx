737a2edd38419ddbd5560e13336774e2d55e3c49
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-12 09:54:06 +0100

    ajout routes à activer par les autres dans App.tsx

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,23 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/home/Home";
+// import Post from "./pages/CreatePost/Post"; à activer
+// import Profile from "./pages/Profile/profile"; à activer ça correspond à /profile/:id
+//import Post from "./pages/DetailPost/post";  à activer ça correspond à DetailPost/post/:id
 import '../src/style.css';
 
 
 
 export default function App() {
     return (
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/home" element={<Home />} />
-            </Routes>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            {/* MS à activer  <Route path="/create/post" element={<Post />} /> */}
+            {/* MS à activer  <Route path="/profile/profile" element={<Profile />} /> */}
+            {/* MS à activer  <Route path="/detailpost/post" element={<Post />} /> */}
+        </Routes>
 
     );
 }
+
+
