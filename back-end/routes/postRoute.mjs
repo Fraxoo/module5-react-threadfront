ff222359cc83ae2fe078dc21840a328e8cd37f78
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-15 16:07:20 +0100

    create post backend fait à 10%

--- back-end/routes/postRoute.mjs
+++ back-end/routes/postRoute.mjs
@@ -11,3 +11,45 @@ router.get("/:postId", getPostById)
 
 
 export default router
+
+
+
+///////
+
+
+//mes codes je réfléchis MS
+
+        // app.post("/posts", isLoggedInJWT(userModel), async (req, res) => {
+        //     console.log(req.body);
+        //     const newPostData = req.body;
+        //     try {
+        //         // +
+        //         const newPost = await Post.create({
+        //             // title: newPostData.title,
+        //             // content: newPostData.content,
+        //             // userId: newPostData.userid, // ça marche !!
+        //             title: req.body.title,
+        //             content: req.body.content,
+        //             UserId: req.userId,
+        //             // PostId:req.postId
+        //         });
+
+        //         res.status(201).json(newPost)
+
+
+        //     } catch (error) {
+        //         console.log(error);
+        //         res.status(500).json({ error: "Erreur lors de la création du post" });
+        //     }
+        // });// ça marche
+        
+        
+        // //mon autre code je réfléchis
+        
+        //     // Création d'un post
+        // const newPost = await Post.create({
+        //     title: "acheter chips",
+        //     content: "pour anniversaire Amaury",
+        //     datetime: new Date(),
+        //     UserId: userById.id
+        // });
