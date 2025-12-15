d618b14377a2af123f02fd66dfc067efa53474d7
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-15 11:24:20 +0100

    commentaire enlevé dans navbar css

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -7,8 +7,8 @@ import Profile from "./pages/profile/Profile";
 import Post from "./pages/post/Post";  
 // à activer ça correspond à DetailPost/post/:id 
 import '../src/style.css';
-import Register from "./pages/Auth/register";
-import Login from "./pages/Auth/login";
+import Register from "./pages/auth/register";
+import Login from "./pages/auth/login";
 //import "./style.css" MS chemin pas bon qui ne vient pas de mon code car le style doit être mius dans src
 
 
@@ -20,12 +20,8 @@ export default function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
-            {/* MS à activer   */}
             <Route path="/createpost/post" element={<CreatePost />} />
-            {/* MS à activer   */}
-            
             <Route path="/profile/profile" element={<Profile />} />
-            {/* MS à activer   */}
             <Route path="/post/post" element={<Post />} />
             <Route path="/register" element={<Register />} />
              <Route path="/login" element={<Login/>} />
