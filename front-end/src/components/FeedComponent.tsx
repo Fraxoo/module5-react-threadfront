492087a7a7e3354ddfedb37904dd03888e8a2a49
Meiko Yahyaoui <meikotoulouse@gmail.com>
2025-12-17 09:44:02 +0100

    WIP: feed component and style updates

--- front-end/src/components/FeedComponent.tsx
+++ front-end/src/components/FeedComponent.tsx
@@ -3,7 +3,7 @@ import type { FeedType } from "../types/FeedType";
 
 export default function FeedComponent<T>({ items, Component }: FeedType<T>) {
  return (
-    <div className="list">
+    <div className="list" >
      
       {items.map((item, index) => (
         <Component
